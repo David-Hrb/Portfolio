c9fd865c52ee412b1f5c77f4b913eea0da80aab8
David-Hrb <hrbacekd08@gmail.com>
2026-02-23 08:18:53 +0100

    logo nahrané

--- app/app.vue
+++ app/app.vue
@@ -4,15 +4,15 @@ useHead({
     { name: 'viewport', content: 'width=device-width, initial-scale=1' }
   ],
   link: [
-    { rel: 'icon', href: '/favicon.ico' }
+    { rel: 'icon', href: '/logo.svg' }
   ],
   htmlAttrs: {
     lang: 'en'
   }
 })
 
-const title = 'Nuxt Starter Template'
-const description = 'A production-ready starter template powered by Nuxt UI. Build beautiful, accessible, and performant applications in minutes, not hours.'
+const title = 'David Hrbacek Portfolio'
+const description = 'Portfolio of David Hrbacek'
 
 useSeoMeta({
   title,
